5038c51b461f20df626a3aad9ff07e45d4fa3e78
VenuPrasathM <venuprasath133@gmail.com>
2024-02-19 08:16:17 -0500

    sample integration of UI with JavaFX

--- src/main/java/xyz/venuprasath/Main.java
+++ src/main/java/xyz/venuprasath/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Factory Pattern!");
         System.out.println("Read more here `https://refactoring.guru/design-patterns/factory-method`");
-        String os = "linux";
+        String os = "windows";
         if(os.equals("linux")) {
             Dialog dialog = new LinuxDialog();
             dialog.createButton();
--- src/main/java/xyz/venuprasath/factory/WindowsButton.java
+++ src/main/java/xyz/venuprasath/factory/WindowsButton.java
@@ -2,14 +2,42 @@ package xyz.venuprasath.factory;
 
 import xyz.venuprasath.factory.interfaces.Button;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class WindowsButton implements Button {
+
+    JPanel panel = new JPanel();
+    JFrame frame = new JFrame();
+    JButton button;
     @Override
     public void render() {
-        System.out.println("Windows button is rendered");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Hello World!");
+        label.setOpaque(false);
+        label.setFont(new Font("Dialog", Font.BOLD, 44));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        frame.getContentPane().add(panel);
+        panel.add(label);
+        onClick();
+        panel.add(button);
+
+        frame.setSize(320, 200);
+        frame.setVisible(true);
+        onClick();
     }
 
     @Override
     public void onClick() {
-        System.out.println("Windows Button is clicked");
+        button = new JButton("Exit");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                System.exit(0);
+            }
+        });
     }
 }
